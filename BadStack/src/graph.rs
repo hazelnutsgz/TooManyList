114ff90d6d40bc86e0ee33f4015339d1986d782e
hazelnutsgz <15201752137@163.com>
2020-01-14 14:41:44 +0800

    Add graph

---
+++ BadStack/src/graph.rs
@@ -0,0 +1,15 @@
+#[derive(Debug)]
+struct Node {
+    nodeId: usize,
+    nodename: String,
+}
+
+#[derive(Debug, Clone)]
+struct Edge {
+    edge: bool,
+}
+
+#[derive(Debug)]
+struct Graphadj {
+    nodenums:
+}
